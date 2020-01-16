8ecc86d2ca495c977793801316defe7ccd881092
yeetlandmc <59981765+yeetlandmc@users.noreply.github.com>
2020-01-16 17:20:52 -0500

    Add files via upload

---
+++ contact-us.php
@@ -0,0 +1,44 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="contact@yeetlandmc.ml";
+    $subject="Your message to us! - ["message"];
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent. We will message you soon!</p>";
+}
+
+?><!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Contact form to email</title>
+</head>
+
+<body>
+
+    <?=$thankYou ?>
+
+    <form method="post" action="contact-us.php">
+        <label>Name:</label>
+        <input name="sender">
+
+        <label>Email address:</label>
+        <input name="Email">
+
+        <label>Message:</label>
+        <textarea rows="5" cols="20" name="message"></textarea>
+
+        <input type="submit" name="submit">
+    </form>
+
+</body>
+
+</html>
